78d65c7af213bb383fc3ac003af48eb7c41d2462
Stevo <stevant@hotmail.de>
2025-03-12 19:37:24 +0100

    add dotnet options pattern

---
+++ docs/dotnet/options-pattern.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 1
+---
+
+# Options Pattern
+
+```csharp title="startup.cs"
+var s = "test"
+```
+
+### Mehr Info unter
+
+https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,65 +4,66 @@ import type * as Preset from '@docusaurus/preset-classic';
 import tailwindPlugin from "./src/plugins/tailwind-config";
 
 const config: Config = {
-  title: 'Docusaurus',
-  favicon: 'img/favicon.ico',
-  url: 'https://docs.stairov.de/',
-  baseUrl: '/',
-  projectName: 'docs', // Usually your repo name.
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-  plugins: [tailwindPlugin],
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    title: 'Docusaurus',
+    favicon: 'img/favicon.ico',
+    url: 'https://docs.stairov.de/',
+    baseUrl: '/',
+    projectName: 'docs', // Usually your repo name.
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+    plugins: [tailwindPlugin],
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Docusaurus',
-      logo: {
-        alt: 'Docusaurus Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Docusaurus',
+            logo: {
+                alt: 'Docusaurus Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Docs',
+                },
+            ],
         },
-      ],
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+            additionalLanguages: ['csharp', 'aspnet']
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
